77439e29a83438c7195be949cec4751b85c9cf6a
Gabriel Novais <silvanovaesgabriel@gmail.com>
2025-06-02 19:46:00 -0300

    Aumentando a borda da resposta do quiz

--- src/scripts/quiz.js
+++ src/scripts/quiz.js
@@ -1,31 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const buttom = document.getElementById('button-iniciar-quiz');
-    const container = document.getElementById('quiz-container');
-    const listquiz = document.querySelectorAll('#quiz-container .quiz')
+  const buttom = document.getElementById('button-iniciar-quiz');
+  const container = document.getElementById('quiz-container');
+  const listquiz = document.querySelectorAll('#quiz-container .quiz')
 
-     buttom.addEventListener('click', function() {
-        buttom.style.display = "none"
-        container.style.display = "flex"
-    });
-    
-    listquiz.forEach(quiz => {
-    const listp = quiz.querySelectorAll("p")
-        var verdadeiro 
-        listp.forEach(p => {
-            if (p.getAttribute("resposta")== "verdadeiro"){
-                verdadeiro = p
-            }
+  buttom.addEventListener('click', function() {
+    buttom.style.display = "none"
+    container.style.display = "flex"
+  });
+  
+  listquiz.forEach(quiz => {
+  const listp = quiz.querySelectorAll("p")
+    var verdadeiro 
+    listp.forEach(p => {
+      if (p.getAttribute("resposta") == "verdadeiro"){
+        verdadeiro = p
+      }
 
-            p.addEventListener("click", function(){
-              if (p.getAttribute("resposta") == "falso"){
-                p.style.border = "1px solid red"
-                verdadeiro.style.border = "1px solid green"
-              }
-               else{
-                  p.style.border = "1px solid green"
-              }
-              quiz.style.pointerEvents = "none"
-        })
+      p.addEventListener("click", function(){
+        if (p.getAttribute("resposta") == "falso"){
+          p.style.border = "2px solid red"
+          verdadeiro.style.border = "2px solid green"
+        } else {
+          p.style.border = "2px solid green"
+        }
+        quiz.style.pointerEvents = "none"
       })
     })
+  })
 });
