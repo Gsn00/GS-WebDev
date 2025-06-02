945aac2c5c2cd24c5914b2d05e137cf1270e5cb9
Gabriel Novais <silvanovaesgabriel@gmail.com>
2025-06-02 17:29:04 -0300

    Criando constantes dos botoes dos slides e fazendo aparecer a primeira imagem

--- src/scripts/slideshow.js
+++ src/scripts/slideshow.js
@@ -4,4 +4,12 @@ document.addEventListener('DOMContentLoaded', function() {
         document.getElementById('slideshow-img-02'),
         document.getElementById('slideshow-img-03')
     ]
+    
+    const arrowLeft = document.getElementById('slide-arrow-left');
+    const arrowRight = document.getElementById('slide-arrow-right');
+
+    let currentImg = 0;
+    let numberOfImages = images.length - 1;
+
+    images[0].style.opacity = '1';
 });
