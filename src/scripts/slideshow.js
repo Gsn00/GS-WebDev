72eaeaaf9a40ffe18b1fda09502390baa95be04e
Gabriel Novais <silvanovaesgabriel@gmail.com>
2025-06-02 17:28:13 -0300

    Listando todas as imagens dos slides

---
+++ src/scripts/slideshow.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const images = [
+        document.getElementById('slideshow-img-01'),
+        document.getElementById('slideshow-img-02'),
+        document.getElementById('slideshow-img-03')
+    ]
+});
